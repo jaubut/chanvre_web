9f7c87b98f1dbbff931c4c4b0f29a03883f9f875
Jeremie Aubut <jeremie.aubut@gmail.com>
2017-01-05 14:48:10 -0500

    adding product like and share
    voila

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,59 +7,44 @@ $(document).ready(function() {
         });
     });
 
-$('#my-size').change(function() {
+    $('#my-size').change(function() {
     $('#my-button').data('item-custom2-value', $(this).val());
-});
+    });
 
-$('#my-quantity').change(function() {
+    $('#my-quantity').change(function() {
     $('#my-button').data('item-quantity', $(this).val());
-});
+    });
 
-
-function myFunction() {
+    function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-}
+    }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
       }
     }
-  }
-}
 
-function myFunctionTwo() {
-    document.getElementById("yourDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    function myFunctionTwo() {
+        document.getElementById("yourDropdown").classList.toggle("show");
     }
-  }
-}
 
-$('#my-price').change(function() {
-    $('#my-button').data('data-item-price', $(this).val());
-});
 
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
-});
+    $('#my-price').change(function() {
+        $('#my-button').data('data-item-price', $(this).val());
+    });
+
+    $('.main-carousel').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true
+    });
