4e5392518490793c66f43e858930e3f545b909ed
Jeremie Aubut <jeremie.aubut@gmail.com>
2017-01-23 21:53:47 -0500

    arrangé larticle
    voila

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -87,15 +87,6 @@
     analytics.page();
   })();
 
-$(document).ready(function() {
-        $(".convert-emoji").each(function() {
-            var original = $(this).html();
-            // use .shortnameToImage if only converting shortnames (for slightly better performance)
-            var converted = emojione.toImage(original);
-            $(this).html(converted);
-        });
-    });
-
     $('#my-size').change(function() {
     $('#my-button').data('item-custom2-value', $(this).val());
     });
