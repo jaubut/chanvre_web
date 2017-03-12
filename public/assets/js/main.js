01faf4d4d7278ea4d2ec60aaf74d5f5ee5afc924
Jeremie Aubut <jeremie.aubut@gmail.com>
2017-03-11 23:19:21 -0500

    adding jquery snippet
    for mobile onclick

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -15,7 +15,24 @@ $('.main-carousel').flickity({
   cellAlign: 'left',
   contain: true
 });
-
+// Add no-touch class to body for mobile touch events and toggle hover class on elements that need it
+	if ("ontouchstart" in document.documentElement) {
+		document.documentElement.className += " touch";
+	}
+	
+	// Add and remove no-hover class to <li>'s for mobile hover events
+	jQuery('.touch .container').each(function() {
+		var div = jQuery(this);
+		
+		div.hover(function() {
+			div.removeClass('no-hover');
+		});
+		
+		jQuery('*').not(div).bind('click', function() {
+			div.addClass('no-hover');
+		});
+		
+	});
 (function(doc, win) {
     var watchArray = [],
         scroll,
