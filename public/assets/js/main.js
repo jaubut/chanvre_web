9e2840d8482fa0c5afab8a6f04c7953dfed6b44c
Jeremie Aubut <jeremie.aubut@gmail.com>
2017-04-18 14:46:15 -0400

    update
    remove onesignal, try moltin, and some other thing

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,9 @@
+var moltin = new Moltin({publicId: 'HimKFkM74zwzb8mtGTBMmCBtw0wVZJ1BUsG0PE9cZ1'});
+var products = moltin.Product.List();
+moltin.Authenticate(function() {
+  // Make your calls here
+});
+
 $('#my-size').change(function() {
 $('#my-button').data('item-custom2-value', $(this).val());
 });
