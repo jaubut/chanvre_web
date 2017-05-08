6e1bc8d67467951da9063289ff997e4f1c8452ee
Samuel Faucher <sn.faucher@gmail.com>
2017-05-07 21:44:06 -0400

    first round of filter feature for product page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -17,4 +17,35 @@ $(document).ready(function () {
         cellAlign: 'left',
         contain: true
     });
+
+    var fuseOptions = {
+        shouldSort: true,
+        threshold: 0.6,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            "name",
+            "headline",
+            "underline",
+            "thirdline",
+            "description",
+            "texte"
+        ]
+    };
+
+    var filteredData = [];
+    $('#filtered-products .product-item').each(function () {
+        filteredData.push($(this).data());
+    });
+    var fuse = new Fuse(filteredData, fuseOptions);
+    $('input.product__filter-search').on('keydown', function (e) {
+        if (e.which == 13) {
+            e.preventDefault();
+            var result = fuse.search($(this).val().trim());
+            console.log('result = ', result);
+        }
+    });
+
 });
